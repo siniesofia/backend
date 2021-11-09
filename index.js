2dc3f9770167f1ecf241edf1530f4415ab065436
Sini <siniesofia@protonmail.com>
2021-11-09 12:47:25 +0200

    morgan toimii

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 app.use(express.json())
+
+
+
+app.use(morgan('tiny'))
+
+
+// const requestLogger = (request, response, next) => {
+//   console.log('Method:', request.method)
+//   console.log('Path:  ', request.path)
+//   console.log('Body:  ', request.body)
+//   console.log('---')
+//   next() 
+// }
+
+// app.use(requestLogger)
 
 let persons = [
     {
@@ -90,6 +106,12 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 
 const PORT = 3001
